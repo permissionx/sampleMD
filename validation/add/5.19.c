0ec696831d9e7ae7cfd84fd8ad7038180d4a68cb
permissionx <buaaxuke@gmail.com>
2023-07-03 03:04:02 +0800

    chapter 5 competed

---
+++ validation/add/5.19.c
@@ -0,0 +1,62 @@
+void Dynamics(double stopTime, double timeStep)
+{
+    double time;
+    int n, d;
+    double temperature;
+    double targetTemperature = 300;
+    FILE *fp;
+    char fileName[50] = "output/5.19_time-temperature.csv";
+    char dumpName[50] = "output/5.19_run.dump";
+
+    InitDynamic();
+
+    time = 0;
+    nStep = 0;
+    fp = fopen(fileName, "w");
+    fprintf(fp, "step time temperature\n");
+    Dump_lammpstrj(dumpName, 1, nStep);
+    while (time <= stopTime)
+    {
+        if (nStep % 100 == 0)
+        {
+            temperature = ComputeTemperature();
+        }
+        if (nStep % 100 == 0)
+        {
+            fprintf(fp, "%d %f %f\n", nStep, time, temperature);
+            printf("%d %f %f\n", nStep, time, temperature);
+            Dump_lammpstrj(dumpName, 0, nStep);
+        }
+        if (nStep >= 4000)
+        {
+            Thermostat(temperature, targetTemperature, 100, timeStep, "Berendsen");
+        }        
+        IterRun(timeStep);
+        nStep += 1;
+        time += timeStep;
+    }
+    fclose(fp);
+}
+
+/* main */
+int main()
+{
+    /* parameters */
+    unsigned int randomSeed;
+    randomSeed = 1;
+    srand(randomSeed);
+
+    typeMasses[1] = 183.84; // for W
+    InitMassUnit();
+    strcpy(potentialName, "EAM");
+    neighborCutoff = 6;
+    neighborInterval = 100;
+    strcpy(dynamicStyle, "VelocityVerlet");
+
+    /* processing*/
+    ConstructStdCrystal_BCC(3.14, 10);
+    InitVelocity(100);
+    Dynamics(1, 0.0001);
+
+    return 0;
+}
--- validation/source/5.19.b.c
+++ validation/source/5.19.b.c
@@ -1626,34 +1626,34 @@ void Dynamics(double stopTime, double timeStep)
     double time;
     int n, d;
     double temperature;
-    double stress[6];
-    double targetStress[3] = {-0.5, -0.5, -0.5};
+    double targetTemperature = 300;
     FILE *fp;
-    char fileName[50] = "output/5.16_time-pressure.csv";
-    char dumpName[50] = "output/5.16_run.dump";
+    char fileName[50] = "output/5.19_time-temperature.csv";
+    char dumpName[50] = "output/5.19_run.dump";
 
     InitDynamic();
 
     time = 0;
     nStep = 0;
     fp = fopen(fileName, "w");
-    fprintf(fp, "step time temperature strxx stryy strzz lx ly lz\n");
-    printf("step time temperature strxx stryy strzz lx ly lz\n");
+    fprintf(fp, "step time temperature\n");
     Dump_lammpstrj(dumpName, 1, nStep);
     while (time <= stopTime)
     {
         if (nStep % 100 == 0)
         {
             temperature = ComputeTemperature();
-            ComputeStress(stress);
         }
         if (nStep % 100 == 0)
         {
-            fprintf(fp, "%d %f %f %f %f %f %f %f %f \n", nStep, time, temperature, stress[0], stress[1], stress[2], boxTranVecs[0][0], boxTranVecs[1][1], boxTranVecs[2][2]);
-            printf("%d %f %f %f %f %f %f %f %f \n", nStep, time, temperature, stress[0], stress[1], stress[2], boxTranVecs[0][0], boxTranVecs[1][1], boxTranVecs[2][2]);
+            fprintf(fp, "%d %f %f\n", nStep, time, temperature);
+            printf("%d %f %f\n", nStep, time, temperature);
             Dump_lammpstrj(dumpName, 0, nStep);
         }
-        Barostat(stress, targetStress, 100, timeStep, "Berendsen");
+        if (nStep >= 4000)
+        {
+            Thermostat(temperature, targetTemperature, 100, timeStep, "Nose-Hoover");
+        }        
         IterRun(timeStep);
         nStep += 1;
         time += timeStep;
@@ -1670,18 +1670,18 @@ int main()
     randomSeed = 1;
     srand(randomSeed);
 
-    typeMasses[1] =   20.1797; // for Ne
+    typeMasses[1] = 183.84; // for W
     InitMassUnit();
-    strcpy(potentialName, "LJ");
-    potentialCutoff_LJ = 10;
-    neighborCutoff = 15;
+    strcpy(potentialName, "EAM");
+    neighborCutoff = 6;
     neighborInterval = 100;
     strcpy(dynamicStyle, "VelocityVerlet");
 
     /* processing*/
-    ConstructStdCrystal_BCC(3.15, 10);
-    InitVelocity(300.0);
-    Dynamics(10.0, 0.0001);
+    ConstructStdCrystal_BCC(3.14, 10);
+    InitVelocity(100);
+    Dynamics(1, 0.0001);
 
     return 0;
 }
+
